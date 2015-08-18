e059f15e3d2d1d48db7651f861d8b8473d5d7db2
michbuett <michbuett@gmx.de>
2015-08-18 16:00:14 +0200

    Initial commit

---
+++ src/js/template.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var path = require('path');
+var loader = require('node-module-loader');
+var templateFile = __dirname + '/../tpl/SpecRunner.tpl';
+var projectRoot = __dirname + '/../../../..';
+
+exports.process = function (grunt, task, context) {
+    var templateSrc = grunt.file.read(templateFile);
+    var modules = [].concat(context.scripts.src).map(function (s) {
+        return ('./' + s).replace(/\/\//g, '/').replace(/\.js$/, '');
+    });
+    // var modules = [].concat(context.scripts.src, context.scripts.specs).map(function (s) {
+    //     return ('./' + s).replace(/\/\//g, '/').replace(/\.js$/, '');
+    // });
+
+    // console.log(JSON.stringify(context, null, '  '));
+
+    loader.build({
+        root: path.join(projectRoot, (context.options.root || '')),
+        modules: modules,
+        target: context.temp + '/loader.js'
+    });
+
+    return grunt.util._.template(templateSrc, context);
+};
---
+++ src/tpl/SpecRunner.tpl
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Jasmine Spec Runner</title>
+  <link rel="shortcut icon" type="image/png" href="<%= temp %>/jasmine_favicon.png">
+<% css.forEach(function(style){ %>
+  <link rel="stylesheet" type="text/css" href="<%= style %>">
+<% }) %>
+
+</head>
+<body>
+<!-- NODE MODULE LOADER -->
+<% with (scripts) { %>
+  <% [].concat(polyfills, jasmine, boot, vendor, helpers, reporters).forEach(function(script){ %>
+  <script src="<%= script %>"></script>
+  <% }) %>
+  <script src="<%= temp %>/loader.js"></script>
+<% }; %>
+</body>
+</html>
